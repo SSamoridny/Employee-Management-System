fc3b55f81eb9174ffb8aaff5ed6cab7209f8afe3
Shaun Samoridny <ssamoridny@gmail.com>
2020-11-04 16:44:03 -0700

    basic connection instructions

--- db.js
+++ db.js
@@ -1,1 +1,21 @@
-const mysql = require ('mysql')
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "Maiyapapaya1!",
+  database: "employeeTracker"
+});
+
+connection.connect(function(err) {
+    console.log('connected')
+    if (err) throw err;
+  });
