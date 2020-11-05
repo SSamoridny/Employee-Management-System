3afc2ec9263ec57d122c8cf694d27c7f065b035d
Shaun Samoridny <ssamoridny@gmail.com>
2020-11-04 17:19:15 -0700

    switched db.js to app.js added express and console.table

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-const inquirer = require('inquirer')
+const mysql = require('mysql');
+const inquirer = require('inquirer');
+const express = require('express')
+const console = require('console.table')
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    // Your port; if not 3306
+    port: 3306,
+    // Your username
+    user: "root",
+    // Your password
+    password: "Maiyapapaya1!",
+    database: "employeeTracker"
+});
+
+connection.connect(function (err) {
+    console.log('connected')
+    if (err) throw err;
+});
