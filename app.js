8207d1abb58974e2517c9c6a338a46806d43386e
Shaun Samoridny <ssamoridny@gmail.com>
2020-11-05 14:20:41 -0700

    Creation of user questions

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
+// Dependencies
 const mysql = require('mysql');
 const inquirer = require('inquirer');
-const express = require('express')
 const console = require('console.table')
 
+// Creation of connection
 const connection = mysql.createConnection({
     host: 'localhost',
     // Your port; if not 3306
@@ -18,3 +19,58 @@ connection.connect(function (err) {
     console.log('connected')
     if (err) throw err;
 });
+
+// Inquirer prompts / questions for user
+
+const userQuestions = function() {
+    inquirer
+      .prompt({
+        type: "list",
+        name: "userQuestions",
+        message: "What would you like to do?",
+        choices: [
+          "view all employees",
+          "view all roles",
+          "view all departments",
+          "add employee",
+          "add department",
+          "add role",
+          "update employee role",
+          "remove employee"
+        ]
+      })
+      .then(function(answer) {
+        console.log(answer);
+        // start of switch statment for user choice
+        switch (answer.userQuestions) {
+          case "view all employees":
+            viewallemployees();
+            break;
+  
+          case "view all roles":
+            viewallroles();
+            break;
+  
+          case "view all departments":
+            viewalldepartments();
+            break;
+  
+          case "add employee":
+            addEmployee();
+            break;
+  
+          case "update employee role":
+            updateEmpRole();
+            break;
+  
+          case "add department":
+            addDepartment();
+            break;
+  
+          case "add role":
+            addRole();
+            break;
+        }
+      });
+  };
+  userQuestions();
